8d74967218172e1e8f9cd7faf78a53645814f9bd
Kristian Rother <kristian@spiced-academy.com>
2024-11-06 09:07:13 +0100

    create Sphinx doc

---
+++ conf.py
@@ -0,0 +1,45 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'SQL Fundamentals'
+copyright = '2024, Kristian Rother'
+author = 'Kristian Rother'
+release = '1.0'
+html_title = f"{project}"
+
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx_design',
+    'sphinx_copybutton',
+    'sphinx.ext.todo',
+    'myst_parser',
+    ]
+
+exclude_patterns = ['experimental', '_build', 'Thumbs.db', '.DS_Store']
+
+language = 'en'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
+html_logo = "_static/academis_logo.png"
+html_favicon = "_static/favicon.ico"
+
+html_css_files = [
+    "academis.css",
+]
+html_theme_options = {
+    "source_repository": "https://github.com/krother/sql_fundamentals",
+    "source_branch": "main",
+    "source_directory": "",
+}
---
+++ index.rst
@@ -0,0 +1,40 @@
+SQL Fundamentals
+================
+
+Material from SQL training for public use
+
+|image0|
+
+Using the data
+--------------
+
+On docker MySQL:
+
+::
+
+   mysql  -h 0.0.0.0 -u root -D babynames --password=3141592
+
+On Hackschule.de
+
+::
+
+   wget ...
+   mysql < babynames.sql
+
+To enter queries:
+
+::
+
+   mycli
+
+Author
+------
+
+(c) 2024 Dr. Kristian Rother and Shreyaasri Prakash
+
+Usable under the conditions of the Creative Commons Attribution
+Share-Alike License 4.0 (CC-BY-SA 4.0). See creativecommons.org for
+details.
+
+.. |image0| image:: https://github.com/krother/sql_fundamentals/blob/00f4c4580be0b5dce7af5958b9f7f0863ec7d208/Images/blue_elephant.png
+
